63c9391368d59fdb1ff091a4ceacd3bf5c2ddb2a
hien <hientdhe163020@gmail.com>
2024-02-08 11:38:24 +0700

    khoi tao project

---
+++ src/main/java/com/in/jplearning/JplearningApplication.java
@@ -0,0 +1,13 @@
+package com.in.jplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JplearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JplearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in/jplearning/JplearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.jplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JplearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
