bde5c63d72366bfb81c54e256e5980d909247bf1
hien <hientdhe163020@gmail.com>
2024-03-11 22:38:40 +0700

    modify enroll course table and enrol course function

---
+++ src/main/java/com/in/jplearning/repositories/CourseEnrollDAO.java
@@ -0,0 +1,15 @@
+package com.in.jplearning.repositories;
+
+import com.in.jplearning.model.Course;
+import com.in.jplearning.model.CourseEnroll;
+import com.in.jplearning.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CourseEnrollDAO extends JpaRepository<CourseEnroll,Long> {
+
+    @Query(value = "select ce from CourseEnroll ce where ce.user.userID =?1 and ce.course.courseID =?2")
+    Optional<CourseEnroll> findByUserAndCourse(Long userID, Long courseID);
+}
